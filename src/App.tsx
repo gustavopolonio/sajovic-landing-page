4c33e57511f4b637e3f8669a091ceba7989a8edb
Gustavo <gustavopolonio1@gmail.com>
2025-02-23 15:11:31 -0300

    seo

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,24 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Index from './pages/Index'
 import NotFound from './pages/NotFound'
+import { HelmetProvider } from 'react-helmet-async'
+import { Seo } from './Seo'
 
 const queryClient = new QueryClient()
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </QueryClientProvider>
+  <HelmetProvider>
+    <Seo />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </HelmetProvider>
 )
 
 export default App
---
+++ src/Seo.tsx
@@ -0,0 +1,29 @@
+import { Helmet } from 'react-helmet-async'
+
+export const Seo = () => {
+  return (
+    <Helmet>
+      <title>Sajovic - Sociedade de Advogados</title>
+      <meta
+        name="description"
+        content="A sociedade de advocacia Sajovic Sociedade de Advogados, inscrita na Ordem dos Advogados do Brasil, Subseção de São Paulo sob n.º 16288, assiste legalmente, desde o ano de 1984, pessoas físicas e jurídicas, atuando nas mais diversas áreas do Direito, objetivando, principalmente, o oferecimento de serviços de alta qualidade na assessoria jurídica aos seus clientes. Nosso forte sempre foi o Direito de Família e Sucessões - Inventários,  Divórcios, Alimentos, Guarda, Planejamento Sucessório (holdings, testamentos). Atuamos com ênfase também na área contratual. O direito do trabalho veio mais presente em 2020 com a formulação de novo quadro societário. As consultas podem ser presenciais ou agendadas de forma telepresencial - videoconferência.  "
+      />
+      <meta
+        name="keywords"
+        content="Sajovic,Sociedade de Advogados,Administrativo,Consumeirista,Direito Bancário e Operações Financeiras,Direito civil,Contratual e Imobiliário,Direito Societário,Extrajudicial,Família e Sucessões,Trabalhista,Advocacia Criminal Especializada,Audiência de custódia,Habeas corpus,Violência doméstica,Assistente de acusação,Advogados,Gabriel Sajovic"
+      />
+      <meta
+        name="author"
+        content="Gustavo Polonio - https://www.linkedin.com/in/gustavo-polonio"
+      />
+
+      <meta property="og:title" content="Sajovic - Sociedade de Advogados" />
+      <meta
+        property="og:description"
+        content="Sajovic,Sociedade de Advogados,Administrativo,Consumeirista,Direito Bancário e Operações Financeiras,Direito civil,Contratual e Imobiliário,Direito Societário,Extrajudicial,Família e Sucessões,Trabalhista,Advocacia Criminal Especializada,Audiência de custódia,Habeas corpus,Violência doméstica,Assistente de acusação,Advogados,Gabriel Sajovic"
+      />
+      <meta property="og:url" content="" />
+      <meta property="og:type" content="website" />
+    </Helmet>
+  )
+}
